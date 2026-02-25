96d0262dc6fc9a51d5ffa9c912c9fe8b9085868e
Yasmin Vale <yasminamorvale@gmail.com>
2026-02-24 22:17:07 -0300

    Aula 1 Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,14 @@
+//Exemplos de variávek let
+
+let nome = 'Yasmin'
+
+let nome2 = 'Nathalia'
+
+console.log(nome);
+console.log(nome2);
+
+//Exemplos de variável const
+const times = ['São Paulo',  'Corinthians', 'Santos'];
+
+
+console.log(times);
