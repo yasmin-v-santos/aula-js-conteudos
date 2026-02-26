ca1e64d55f44d6ae26e0808b9c711e938537e623
Yasmin Vale <yasminamorvale@gmail.com>
2026-02-25 22:21:16 -0300

    Aula 25.02.2026

---
+++ Aula2/src/script02.js
@@ -0,0 +1,26 @@
+//Exeplos de operações aritméticas
+let a = 5;
+let x = (100 + 200) * a;
+
+console.log(x);
+
+//Exemplo de adição
+let texto1 = "Somente a dor coletiva ";
+
+texto1 = texto1 + 'gera a união!'
+
+//texto1 += 'gera a união!';
+
+console.log(texto1);
+
+//Exemplo de Incremento
+let w = 5;
+w++;
+let z = w;
+console.log(z);
+
+//Exemplo de Decremento
+let b = 5;
+b--;
+let c = b;
+console.log(c);
