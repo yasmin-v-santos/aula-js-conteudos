0b724a85ed06a9b4c9c795476f5bf0c57a314124
Yasmin Vale <yasminamorvale@gmail.com>
2026-03-02 22:17:27 -0300

    Aula 5 - Laço de Repetição

---
+++ Aula5/src/script02.js
@@ -0,0 +1,12 @@
+// Exemplo do operador NOT
+let usuarioLogado = false;
+
+if (!usuarioLogado) {
+    // console.log('teste');
+}
+
+let lista = [];
+
+if (!lista.length) {
+    console.log('A lista está vazia')
+}
---
+++ Aula5/src/script03.js
@@ -0,0 +1,24 @@
+// Introdução Laços de Repetição
+
+//Exemplo de For Loop
+let valor = 20;
+
+for (let valor = 0; valor < 10; valor++){
+    // console.log(valor);
+}
+
+// Exemplo de While Loop
+// let acumuladora = 0;
+
+// while (acumuladora <= 10) {
+    // console.log("O valor da acumuladora agora é de: " + acumuladora);
+    // acumuladora = acumuladora + 1;
+// }
+
+// Exemplo de Do While Loop
+let acumuladora2 = 0;
+
+do {
+    console.log("O valor da acumuladora agora é de: " + acumuladora2);
+    acumuladora2 = acumuladora2 + 1;
+}   while (acumuladora2 < 10);
